8cc4de78017ee83fa5b4565d8fa9eedd24ded5a9
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-11 12:10:58 +0200

    inserting many rows in table customers

--- task_6.sql
+++ task_6.sql
@@ -7,7 +7,7 @@ USE alx_book_store;
 # Insert data into table customers
 INSERT INTO customers(customer_id, customer_name, email, address)
 VALUES
-(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+(2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave."),
 
 (3, 'Obed Ehoneah', 'eabed@sandtech.com', '125 Happiness Ave.'),
 
