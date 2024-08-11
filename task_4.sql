6a35730069872f566edc22e396c23af4f18b14ea
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-11 10:55:41 +0200

    show table description

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+## Write a script that prints the full description of the table books from the database alx_book_store in your MySQL server.
+
+## let use the database alx_book_store
+USE alx_book_store;
+
+# let print table books description
+SHOW columns from books;
