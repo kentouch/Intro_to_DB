93a42c2c9577225ac23d371261e1373c5dc6f191
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-11 11:45:22 +0200

    inserting many rows in table customers

---
+++ task_6.sql
@@ -0,0 +1,19 @@
+## Populating Your Database with Your More Data
+
+# use the same database 'alx_book_store'
+USE alx_book_store;
+
+# inserting many rows of records
+# Insert data into table customers
+INSERT INTO customers(customer_id, customer_name, email, address)
+VALUE(2, 'Blessing Malik', 'bmalik@sandtech.com', '123 Happiness Ave.');
+
+# inserting many rows of records
+# Insert data into table customers
+INSERT INTO customers(customer_id, customer_name, email, address)
+VALUE(3, 'Obed Ehoneah', 'eabed@sandtech.com', '123 Happiness Ave.');
+
+# inserting many rows of records
+# Insert data into table customers
+INSERT INTO customers(customer_id, customer_name, email, address)
+VALUE(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '123 Happiness Ave.');
