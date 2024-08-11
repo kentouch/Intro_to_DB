5855ceac15d5658d0d53fdffaa791e0730decab2
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-11 10:27:49 +0200

    creating tables in alx_book_store

---
+++ task_2.sql
@@ -0,0 +1,41 @@
+## Write a script that creates all the tables below in alx_book_store in your MySQL server.
+
+#Books: Stores information about books available in the bookstore.
+CREATE TABLE Books (
+    book_id INT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    FOREIGN KEY (author_id) REFERENCING Authors(author_id),
+    price DOUBLE NOT NULL,
+    publication_date DATE
+);
+
+#Authors: Stores information about authors.
+CREATE TABLE Authors (
+    author_id INT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL,
+    
+);
+
+#Customers: Stores information about customers.
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215),
+    address TEXT
+);
+
+#Orders: Stores information about orders placed by customers
+CREATE TABLE Orders (
+    order_id INT PRIMARY KEY,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+); 
+
+#Order_Details: Stores information about the books included in each order.
+CREATE TABLE Order_Details (
+    order_detail_id INT PRIMARY KEY,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
