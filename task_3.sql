ecd416d4c493bde2f67e134d3e1a50165ab3c465
Ruzindana Kent <ruzindanakent.rk@gmail.com>
2024-08-11 10:33:35 +0200

    listing all the tables

---
+++ task_3.sql
@@ -0,0 +1,7 @@
+## Write a script that list all the tables in alx_book_store in your MySQL server.
+
+## let use the database alx_book_store
+USE alx_book_store;
+
+## List all the tables in alx_book_store
+SHOW TABLES;
